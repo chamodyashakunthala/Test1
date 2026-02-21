cfe1a91514f55f6758aaa75cc55a25fed04ebcbe
chamodyashakunthala <shakunthalachamodya@gmail.com>
2026-02-21 22:39:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from datetime import datetime, timedelta
 import json
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 app = Flask(__name__)
 CORS(app)
 
@@ -189,4 +195,5 @@ def get_today_schedule():
     })
 
 if __name__ == '__main__':
+
     app.run(debug=True, port=5000)
